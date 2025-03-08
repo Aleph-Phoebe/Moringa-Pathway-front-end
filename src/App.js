0a6dcefa22b1f19cc224036f377406988872fd98
Aleph Phoebe <phbwangui47@gmail.com>
2025-03-08 22:03:19 +0300

    initial update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AdminDashboard from './pages/AdminDashboard';
+import AdminDashboard from './pages/admin/AdminDashboard';
 import UserDashboard from './pages/UserDashboard';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
@@ -9,12 +9,13 @@ import ResourcesPage from './pages/ResourcesPage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
 import JobsPage from './pages/JobsPage';
+import PremiumPage from './pages/PremiumPage';
+import Payment from './components/Payment'; // Payment Component
 import ProtectedRoute from './components/ProtectedRoute';
 import AdminRoute from './components/AdminRoute';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import PremiumPage from './pages/PremiumPage';
-import { AuthProvider } from './contexts/AuthContext'; // Ensure correct import
+import { AuthProvider } from './contexts/AuthContext';
 
 const App = () => {
   return (
@@ -29,9 +30,12 @@ const App = () => {
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/jobs" element={<JobsPage />} />
-          <Route path="/admin/*" element={<AdminRoute><AdminDashboard /></AdminRoute>} />
+          
           <Route path="/dashboard/*" element={<ProtectedRoute><UserDashboard /></ProtectedRoute>} />
           <Route path="/premium" element={<ProtectedRoute><PremiumPage /></ProtectedRoute>} />
+          <Route path="/payment" element={<ProtectedRoute><Payment /></ProtectedRoute>} /> {/* Payment Page */}
+
+          <Route path="/admin/*" element={<AdminRoute><AdminDashboard /></AdminRoute>} />
         </Routes>
         <Footer />
       </Router>
